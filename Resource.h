fa9ca034918facff849b567406113e8ef33ac4bd
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2025-04-10 05:27:29 +0200

    Added Resource.h

---
+++ Resource.h
@@ -0,0 +1,45 @@
+#pragma once
+
+enum Resource_Type
+{
+	Electro_Device , 
+	Ultrasound_Device ,
+	Room
+};
+
+class Resource
+{
+protected:
+	Resource_Type Type;
+	const int Id;
+	bool Availability;
+
+public:
+	Resource(Resource_Type RT , int ID ) : Type(RT), Id(ID), Availability(1)
+	{
+		//Initializing Id here is random ? Only God Knows and Mo3eda (Maybe)
+		// No Need for setters for Type & ID
+	}
+
+	Resource_Type Get_Type()
+	{
+		return Type;
+	}
+
+	int Get_Id()
+	{
+		return Id;
+	}
+
+	void Set_Availability(bool Statue)
+	{
+		Availability = Statue;
+	}
+
+	bool Get_Availability()
+	{
+		return Availability;
+	}
+
+
+};
