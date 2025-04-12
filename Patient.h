b85289824fd100dd1d001381e7f21a86b0eb3cf2
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2025-04-12 02:48:53 +0200

    Added Setters and Getters to Patient and Enqueue Treatment with Dauration Time

--- Patient.h
+++ Patient.h
@@ -13,17 +13,64 @@ protected:
 	Treatment* CurrTreatment;// wasn't mentioned by TAs
 
 public:
-	void SetCurrTreatment(Treatment* c)
+
+	Patient()
 	{
-		CurrTreatment = c;
+		 ID = 0;
+		 PT = 0;
+		 VT = 0;
+		 Type = ' ';
 	}
-	Treatment* GetCurrTreatment()
+
+	Patient(int d, int pt, int vt, char type) : ID(d) , PT(pt) , VT(vt) , Type(type)
 	{
-		return CurrTreatment;
+		//CurrTreatment == nullptr;
 	}
+
+	void Set_ID(int d)
+	{
+		ID = d;
+	}
+
+	void Set_PT(int pt)
+	{
+		PT = pt;
+	}
+
 	int getPT() const
 	{
 		return PT;
 	}
-};
 
+	void Set_VT(int vt)
+	{
+		VT = vt;
+	}
+
+	int getVT() const
+	{
+		return VT;
+	}
+
+	void Set_Type(char T)
+	{
+		Type = T;
+	}
+
+	void Enqueue_ReqTreatment(Treatment* treatment , int d )
+	{
+		treatment->Set_Duration(d);
+		ReqTreatmentList.enqueue(treatment);
+	}
+
+	void SetCurrTreatment(Treatment* c)
+	{
+		CurrTreatment = c;
+	}
+
+	Treatment* GetCurrTreatment()
+	{
+		return CurrTreatment;
+	}
+
+};
