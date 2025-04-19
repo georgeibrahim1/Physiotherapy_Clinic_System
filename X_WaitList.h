1843976095c2e858e58298d3e72b103bd47e3e21
ehab <ehab@DESKTOP-6K3F9V9>
2025-04-19 09:57:14 +0200

    X waiting list problem solved

--- X_WaitList.h
+++ X_WaitList.h
@@ -16,48 +16,61 @@ public:
 
 bool X_WaitList::cancel(Patient*& to_delete)
 {
-	if (LinkedQueue <Patient*> ::isEmpty())
-		return false;
 
-
+	for (int i = 0; i < (this->getcount() * 3); i++) // loop multiple times with different index
+	{
 		srand(time(0));
+
+		if (LinkedQueue <Patient*> ::isEmpty())
+			return false;
 
 		int to_cancel = rand() % this->getcount();
 		//loop to search for the patient to remove
 		//delete this node then connect previous node to next
 		//make a temp pointer to that node and enqueue it to finish list
 
-		             
+
 		Node<Patient*>* curr = this->frontPtr;
-		Node<Patient*>* prev = this->frontPtr;   
+		Node<Patient*>* prev = nullptr;
+
 
 
 		for (int j = 0; j < to_cancel; j++)
 		{
+			prev = curr;
 			curr = curr->getNext();
-		}
-		for (int j = 0; j < to_cancel - 1; j++)
-		{
-			prev = prev->getNext();
+
 		}
 
 		Treatment* x;
 		curr->getItem()->Get_reqtreatmentlist().peek(x);
 		if (curr->getItem()->Get_reqtreatmentlistcount() == 1 && x->get_type() == 'X')   // If the count of remaining treatments is one, then X_exercise is the last treatment for sure.
 		{
-			if (!(curr == this->frontPtr))
-				prev->setNext(curr->getNext());
-			else
+			if (curr == prev) // Single Node
+			{
+				this->frontPtr = nullptr;
+				this->backPtr = nullptr;
+			}
+			else if (curr == this->frontPtr) // Front Node
 				this->frontPtr = curr->getNext();
+			else if (curr == this->backPtr) // Back Node
+			{
+				this->backPtr = prev;
+				prev->setNext(nullptr);
+			}
+			else
+				prev->setNext(curr->getNext()); // General case
+
 			to_delete = curr->getItem();
 			count--;
+			curr->setNext(nullptr);
 			delete curr;
-			
 			return true;
 		}
+	}
 	return false;
 	//After deleting the patient, he should be moved to the finish list(push(dataentry))
 	// after deleting, you should decrment count
 
 
-}              
+}
